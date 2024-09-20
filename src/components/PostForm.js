2403c3e60b82fc4cfed50bea58e4ae702ad6f873
DuutraXz <162321740+DuutraXz@users.noreply.github.com>
2024-09-19 22:19:22 -0300

    Adicionando comentários ao componente PostForm.js

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -34,9 +34,11 @@ const FormContainer = styled.form`
   }
 `;
 
+// Componente de formulário para criar uma nova postagem
 class PostForm extends React.Component {
   constructor(props) {
     super(props);
+    // Estado inicial do formulário
     this.state = {
       title: '',
       content: '',
@@ -44,22 +46,26 @@ class PostForm extends React.Component {
     };
   }
 
+  // Mudança no campo de título
   onChangeTitle = (event) => {
     this.setState({ title: event.target.value });
   }
   
+  // Mudança no campo de conteúdo
   onChangeContent = (event) => {
     this.setState({ content: event.target.value });
   }
   
+  // Mudança no campo de URL da imagem
   onChangeImageUrl = (event) => {
     this.setState({ imageUrl: event.target.value });
   }
   
-
+  // Enviar formulário
   onSubmit = (event) => {
-    event.preventDefault();
-    this.props.onAddPost(this.state);
+    event.preventDefault(); // Evita recarregar a página
+    this.props.onAddPost(this.state); // Chama a função passada via props para adicionar a postagem
+    // Reseta os campos do formulário após envio do formulário
     this.setState({ title: '', content: '', imageUrl: '' });
   }
 
@@ -72,15 +78,15 @@ class PostForm extends React.Component {
           placeholder="Título"
           value={this.state.title}
           onChange={this.onChangeTitle}
-          maxLength="50"
-          required
+          maxLength="50" // Limita o título a 50 caracteres
+          required // Campo obrigatório
         />
         <textarea
           name="content"
           placeholder="Conteúdo"
           value={this.state.content}
           onChange={this.onChangeContent}
-          required
+          required // Campo obrigatório
         />
         <input
           type="url"
