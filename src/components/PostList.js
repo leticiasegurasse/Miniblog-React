b3627241291f6567639cc7f7bdc4840b3ce25ea3
Leticia Segurasse <146223401+leticiasegurasse@users.noreply.github.com>
2024-09-19 22:14:47 -0300

    Adicionando comentários ao componente PostList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,19 +6,19 @@ const PostListContainer = styled.div`
   width: 100%;
 `;
 
-
 class PostList extends React.Component {
   render() {
-    const { posts, onDelete, onReadMore } = this.props;
+    const { posts, onDelete, onReadMore } = this.props; // Desestruturação das props recebidas
 
     return (
       <PostListContainer>
+        {/* Itera sobre o array de posts e renderiza um PostItem para cada post */}
         {posts.map((post) => (
           <PostItem
-            key={post.id}
-            post={post}
-            onDelete={onDelete}
-            onReadMore={onReadMore}
+            key={post.id} 
+            post={post} 
+            onDelete={onDelete} 
+            onReadMore={onReadMore} 
           />
         ))}
       </PostListContainer>
